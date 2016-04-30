a95cbc5a85fb909b75e472464f662cd2e619fe7c
why <why123@163.com>
2016-04-30 19:21:22 +0800

    创建了一条狗

---
+++ Git练习/Git练习/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  Git练习
+//
+//  Created by isoftstone on 16/4/30.
+//  Copyright © 2016年 com.hao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ Git练习/Git练习/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  Git练习
+//
+//  Created by isoftstone on 16/4/30.
+//  Copyright © 2016年 com.hao. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
