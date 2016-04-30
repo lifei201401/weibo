6b4ee1ed466b68c296203ac3ab820574ebb0b111
why <why123@163.com>
2016-04-30 19:17:28 +0800

    初始化远程仓库

---
+++ Git练习/Git练习/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git练习
+//
+//  Created by isoftstone on 16/4/30.
+//  Copyright © 2016年 com.hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git练习/Git练习/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git练习
+//
+//  Created by isoftstone on 16/4/30.
+//  Copyright © 2016年 com.hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
